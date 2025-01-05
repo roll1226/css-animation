464cec8abe5b5e5c1405214964e48119fdf27f41
roll1226 <tomo_yasu_4724@icloud.com>
2025-01-06 07:54:00 +0900

    feat: example page

---
+++ src/pages/example/index.ts
@@ -0,0 +1,5 @@
+document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
+  <div>
+    <h1>Example</h1>
+  </div>
+`;
--- src/path.ts
+++ src/path.ts
@@ -3,6 +3,7 @@ export const PATH = {
   ANIMATION1: "/animation1",
   ANIMATION2: "/animation2",
   ANIMATION3: "/animation3",
+  EXAMPLE: "/example",
   NOT_FOUND: "/404",
 };
 
@@ -11,5 +12,6 @@ export const PATHS = {
   [PATH.ANIMATION1]: () => import("./pages/animation1"),
   [PATH.ANIMATION2]: () => import("./pages/animation2"),
   [PATH.ANIMATION3]: () => import("./pages/animation3"),
+  [PATH.EXAMPLE]: () => import("./pages/example"),
   [PATH.NOT_FOUND]: () => import("./pages/notFound"),
 };
