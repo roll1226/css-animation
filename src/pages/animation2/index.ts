def7bde1a1a0677eee7bc71f05be11dc05b4296c
roll1226 <tomo_yasu_4724@icloud.com>
2025-01-28 07:26:59 +0900

    feat: add animation2 page

--- src/pages/animation2/index.ts
+++ src/pages/animation2/index.ts
@@ -1,13 +1,16 @@
 import "./style.scss";
 
 document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
-  <div>
+  <div class='animation2'>
     <div></div>
     <div></div>
     <div></div>
     <div></div>
-    <div>
-      テキスト
+    <div class='text'>
+      <div>テ</div>
+      <div>キ</div>
+      <div>ス</div>
+      <div>ト</div>
     </div>
   </div>
 `;
