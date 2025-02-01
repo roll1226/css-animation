1e6c3192146d3276e51ac3a2ffa19dfd27b1916d
roll1226 <tomo_yasu_4724@icloud.com>
2025-02-01 10:04:35 +0900

    feat: add animation3

--- src/pages/animation3/index.ts
+++ src/pages/animation3/index.ts
@@ -1,7 +1,14 @@
 import "./style.scss";
 
 document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
-  <div>
-    <h1>Animation 3</h1>
+  <div class="animation3">
+    <div>テ</div>
+    <div>テ</div>
+    <div>キ</div>
+    <div>キ</div>
+    <div>ス</div>
+    <div>ス</div>
+    <div>ト</div>
+    <div>ト</div>
   </div>
 `;
